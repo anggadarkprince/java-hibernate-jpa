072542fd760d1fc73efbc3317d66f83776a3d899
Angga Ari Wijaya <anggadarkprince@gmail.com>
2015-12-05 21:11:00 +0700

    query sql build-in function

---
+++ src/main/java/com/sketchproject/myhibernate/data/HqlFunction.java
@@ -0,0 +1,41 @@
+package com.sketchproject.myhibernate.data;
+
+import com.sketchproject.myhibernate.entities.Account;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class HqlFunction {
+
+    @SuppressWarnings({"unchecked"})
+    public static void main(String[] args) {
+        SessionFactory factory = null;
+        Session session = null;
+        org.hibernate.Transaction tx = null;
+
+        try {
+            factory = HibernateUtil.getSessionFactory();
+            session = factory.openSession();
+            tx = session.beginTransaction();
+
+            Query query = session.createQuery("select distinct t.account from Transaction t"
+                    + " where t.amount > 500 and lower(t.transactionType) = 'deposit'");
+
+            List<Account> accounts = query.list();
+
+            for (Account a : accounts) {
+                System.out.println(a.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
